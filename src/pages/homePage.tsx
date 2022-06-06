3dd166e0e5d47bc53d4913f576616ead4483c142
JoseRivero <josearmandorr95@gmail.com>
2022-06-06 11:42:28 +0200

    Add componentes/ Footer&Header

--- src/pages/homePage.tsx
+++ src/pages/homePage.tsx
@@ -1,10 +1,24 @@
+import { Footer } from '../components/footer/footer';
+import { Header } from '../components/header/header';
+
 export function HomePage() {
     const template = (
         <>
+            <Header></Header>
             <div>
-                {' '}
-                <h1> Rick and Morty</h1>
+                <p> rick and morty</p>
             </div>
+            <div>
+                <p>rick and morty</p>
+            </div>
+            <div>
+                <p>rick and morty</p>
+            </div>
+            <div>
+                <p>rick and morty</p>
+            </div>
+
+            <Footer></Footer>
         </>
     );
 
