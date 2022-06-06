2d77392b0de8b59e54d8b965f5f71fe4ac660c66
Jorge Alia <jorgealia94@gmail.com>
2022-06-06 16:30:50 +0200

    Add header test

--- src/components/footer/footer.spec.tsx
+++ src/components/footer/footer.spec.tsx
@@ -4,7 +4,7 @@ import { Footer } from './footer';
 
 describe('Given the component Footer', () => {
     describe('When calling it', () => {
-        test('Render FooterDesktop', () => {
+        test('Render Footer', () => {
             render(<Footer />);
             const nameTeam = screen.getByText(/J´s Scripts/i);
             const copyright = screen.getByText(/All the data in/i);
