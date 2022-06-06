6dfc94598b56b2fbf3219f03e8495f1c69ada096
JoseRivero <josearmandorr95@gmail.com>
2022-06-06 11:26:32 +0200

    Add Home page & fav icon

---
+++ src/pages/homePage.tsx
@@ -0,0 +1,12 @@
+export function HomePage() {
+    const template = (
+        <>
+            <div>
+                {' '}
+                <h1> Rick and Morty</h1>
+            </div>
+        </>
+    );
+
+    return template;
+}
