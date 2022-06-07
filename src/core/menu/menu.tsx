4854ae979fce82b89b8c108715ae764bcdd19a4c
Jorge Alia <jorgealia94@gmail.com>
2022-06-07 09:54:04 +0200

    Update menu icons

--- src/core/menu/menu.tsx
+++ src/core/menu/menu.tsx
@@ -1,3 +1,4 @@
+import './menu.css';
 import { Link } from 'react-router-dom';
 import { aMenuItems } from '../../interfaces/menu-items';
 
@@ -7,9 +8,24 @@ export function Menu({ options }: { options: aMenuItems }) {
             <ul>
                 {options.map((item) => (
                     <li key={item.label}>
-                        <Link to={item.path}>{item.label}</Link>
+                        <Link to={item.path}>
+                            <img
+                                className="menu__icon"
+                                src={`./img/icon` + item.label + `.png`}
+                                alt=""
+                            />
+                        </Link>
                     </li>
                 ))}
+                <li>
+                    <Link to="login">
+                        <img
+                            className="menu__icon"
+                            src={`./img/iconlogin.png`}
+                            alt=""
+                        />
+                    </Link>
+                </li>
             </ul>
         </nav>
     );
