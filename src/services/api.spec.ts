ab04efd194d32cf7316647d20da48f4cc3e9fba4
Javierfg86 <Javierfg86@outlook.es>
2022-06-08 16:48:35 +0200

    Change testing on store

--- src/services/api.spec.ts
+++ src/services/api.spec.ts
@@ -5,14 +5,20 @@ describe('Given characterApi', () => {
         describe('And we use method getCharacters', () => {
             test('Then the fetch should return an array of 2 characters', async () => {
                 global.fetch = jest.fn().mockResolvedValue({
-                    json: jest
-                        .fn()
-                        .mockResolvedValue([
-                            new CharacterApi(),
-                            new CharacterApi(),
-                        ]),
+                    json: jest.fn().mockResolvedValue([
+                        {
+                            id: 1,
+                            name: 'Rick Sanchez',
+                            status: 'Alive',
+                        },
+                        {
+                            id: 2,
+                            name: 'Rick Sanchez',
+                            status: 'Alive',
+                        },
+                    ]),
                 });
-                const result = await CharacterApi.getCharacters();
+                const result = await CharacterApi.getCharacters(1);
                 expect(fetch).toBeCalled();
                 expect(result.length).toBe(2);
             });
