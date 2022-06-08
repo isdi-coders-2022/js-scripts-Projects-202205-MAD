795241d26c4b5c4306ab6acbd0cc9d8614e109ed
JoseRivero <josearmandorr95@gmail.com>
2022-06-08 18:18:25 +0200

    Update/buttonfav & deletebutton

--- src/components/buttons/favButtons.tsx
+++ src/components/buttons/favButtons.tsx
@@ -5,14 +5,20 @@ import { iCharacter } from '../../interfaces/interfaz';
 import './favbutton.css';
 
 export function FavButtons({ character }: { character: iCharacter }) {
-    const { addCharacter } = useContext(CharactersContext);
+    const { addCharacter, charactersFav, deleteCharacter } =
+        useContext(CharactersContext);
+
     const fav = { ...character, favorite: true };
 
     const template = (
         <>
             <div
                 role="button"
-                className="button__fav"
+                className={
+                    charactersFav.find((item) => item.id === character.id)
+                        ? 'button__fav favorite'
+                        : 'button__fav '
+                }
                 onClick={() => {
                     addCharacter(fav);
                 }}
