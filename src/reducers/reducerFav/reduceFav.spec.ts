61ce72e9b00a8d48c0e54b34f05ce7c1aa6eb17e
Jorge Alia <jorgealia94@gmail.com>
2022-06-09 15:31:55 +0200

    Update test reducer

--- src/reducers/reducerFav/reduceFav.spec.ts
+++ src/reducers/reducerFav/reduceFav.spec.ts
@@ -15,40 +15,38 @@ import { characterFavReducer } from './reducefav';
 
 // Repito los pasos por cada tipo de acción más el default
 
-// describe('first', () => {
-//     describe('first', () => {
-//         const mockInitialState: Array<iCharacter> = [];
+describe('first', () => {
+    describe('first', () => {
+        //         const mockInitialState: Array<iCharacter> = [];
 
-//         const character = {
-//             id: 24,
-//             name: 'Armagheadon',
-//             status: 'Alive',
-//             species: 'Alien',
-//             type: 'Cromulon',
-//             gender: 'Male',
-//             origin: {
-//                 name: 'Signus 5 Expanse',
-//                 url: 'https://rickandmortyapi.com/api/location/22',
-//             },
-//             location: {
-//                 name: 'Signus 5 Expanse',
-//                 url: 'https://rickandmortyapi.com/api/location/22',
-//             },
-//             image: 'https://rickandmortyapi.com/api/character/avatar/24.jpeg',
-//             episode: ['https://rickandmortyapi.com/api/episode/16'],
-//             url: 'https://rickandmortyapi.com/api/character/24',
-//             created: '2017-11-05T08:48:30.776Z',
-//             favorite: true,
-//         };
-//         test('should first', () => {
-//             const [mockInitialState, dispatch] = useReducer(
-//                 characterFavReducer,
-//                 mockInitialState
-//             );
-
-//             addCharactersFavAction;
-
-//             expect(mockInitialState.length).toBe(1);
+        //         const character = {
+        //             id: 24,
+        //             name: 'Armagheadon',
+        //             status: 'Alive',
+        //             species: 'Alien',
+        //             type: 'Cromulon',
+        //             gender: 'Male',
+        //             origin: {
+        //                 name: 'Signus 5 Expanse',
+        //                 url: 'https://rickandmortyapi.com/api/location/22',
+        //             },
+        //             location: {
+        //                 name: 'Signus 5 Expanse',
+        //                 url: 'https://rickandmortyapi.com/api/location/22',
+        //             },
+        //             image: 'https://rickandmortyapi.com/api/character/avatar/24.jpeg',
+        //             episode: ['https://rickandmortyapi.com/api/episode/16'],
+        //             url: 'https://rickandmortyapi.com/api/character/24',
+        //             created: '2017-11-05T08:48:30.776Z',
+        //             favorite: true,
+        //         };
+        test('should first', () => {
+            //             const [mockInitialState, dispatch] = useReducer(
+            //                 characterFavReducer,
+            //                 mockInitialState
+            //             );
+            //             addCharactersFavAction;
+            //             expect(mockInitialState.length).toBe(1);
         });
     });
 });
